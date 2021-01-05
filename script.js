6a03ee8e1f0b9c50f911b640d1a73296f4967c28
SevinchPulatova3724 <77002613+SevinchPulatova3724@users.noreply.github.com>
2021-01-05 17:31:33 +0500

    first

---
+++ script.js
@@ -0,0 +1,17 @@
+var name = prompt('Введите своё имя');
+var year = +prompt('Введи свой возрост');
+var ex1 = +prompt('Решите 50 + 50 = ?');
+var ex2 = +prompt('Решите 100 - 50 = ?');
+var ex3 = +prompt('Решите 2 * 50 = ?');
+var ex4 = +prompt('Решите 100 / 2 = ?');
+var ex5 = +prompt('Решите 100 % 2 = ?');
+  
+  
+console.log('Меня зовут ' + name);
+console.log('Мне ' + year);
+console.log('№1 50 + 50 = ' + (50 + 50) + ' Ваш ответ ' + ex1);
+console.log('№2 100 - 50 = ' + (100 - 50) + ' Ваш ответ ' + ex2);
+console.log('№3 2 * 50 = ' + (2 * 50) + ' Ваш ответ ' + ex3);
+console.log('№4 100 / 2 = ' + (100 / 2) + ' Ваш ответ ' + ex4);
+console.log('№5 100 % 2 = ' + (100 % 2) + ' Ваш ответ ' + ex5);
+
